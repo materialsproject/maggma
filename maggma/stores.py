93f93bfe98ec3b2cc5e4390200b61885aa69790c
Kiran Mathew <km468@cornell.edu>
2017-06-15 07:03:49 -0700

    fix /cleanup stores module

--- maggma/stores.py
+++ maggma/stores.py
@@ -1,68 +1,75 @@
 import abc
 import datetime
-
 import mongomock
-import pymongo
 import six
+import json
+
+import pymongo
+
+from monty.json import MSONable
+
 
 @six.add_metaclass(abc.ABCMeta)
 class Store(MSONable):
+    def __init__(self, lu_field='_lu'):
+        self.lu_field = lu_field
 
-	def __init__(self,lu_field='_lu'):
-		self.lu_field = lu_field
+    @property
+    @abc.abstractmethod
+    def collection(self):
+        pass
 
-	@property
-	@abc.abstractmethod
-	def collection(self):
-		pass
+    @abc.abstractmethod
+    def last_updated(self):
+        pass
 
-	@abc.abstractmethod
-	last_updated(self):
-		pass
+    def lu_fiter(self, targets):
+        """
+        Assuming targets is a list of stores
+        """
+        if isinstance(targets, Store):
+            targets = [targets]
 
-	@classmethod
-	def lu_fiter(cls, targets):
-		"""
-		Assuming targets is a list of stores
-		"""
-		if isinstance(targets, Store):
-			targets = [targets]
+        lu_list = [t.last_updated for t in targets]
+        return {self.lu_field: {"$gte": max(lu_list)}}
 
-		lu_list = [t.last_updated for t in targets]
-		return  {self.lu_field: {"$gte": max(lu_list)}}
+    def __eq__(self, other):
+        return self.as_dict() == other.as_dict()
+
+    def __ne__(self, other):
+        return not self == other
 
 
 class MongoStore(Store):
+    def __init__(self, collection, lu_field='_lu'):
+        self._collection = collection
+        super(MongoStore, self).__init__(lu_field)
 
-	def __init__(self, collection, lu_field='_lu'):
-		self._collection = collection
-		super().__init__(lu_field)
+    def collection(self):
+        return self._collection
 
-	def collection(self):
-		return _collection
-
-	def last_updated(self):
-        doc = next(_collection.find({}, {"_id": 0, self.lu_field: 1}).sort(
+    def last_updated(self):
+        doc = next(self._collection.find({}, {"_id": 0, self.lu_field: 1}).sort(
             [(self.lu_field, pymongo.DESCENDING)]).limit(1), None)
         return doc[self.lu_field] if doc else datetime.datetime.min
 
 
 class JSONStore(MongoStore):
+    def __init__(self, path, lu_field='_lu'):
+        self.path = path
+        _collection = mongomock.MongoCient().db.collection
 
-	def __init__(self, path, lu_field='_lu'):
-		self.path = path
-		_collection = mongomock.MongoCient().db.collection
+        with open(path) as f:
+            objects = list(json.load(f))
+            objects = [objects] if not isinstance(objects, list) else objects
+            _collection.insert_many(objects)
 
-		with open(path) as f:
-			objects = list(json.load(f))
-			objects = [objects] if not isinstance(objects,list) else objects
-			_collection.insert_many(objects)
-
-		super().__init__(_collection,lu_field)
+        super().__init__(_collection, lu_field)
 
 
 class DatetimeStore(MongoStore):
     """Utility store intended for use with `Store.lu_filter`."""
+
     def __init__(self, dt, lu_field='_lu'):
         self._dt = dt
         collection = mongomock.MongoClient().db.collection
