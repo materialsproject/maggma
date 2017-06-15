7cb12512fa9b486e90eac64e00cf2714d15d8367
Kiran Mathew <km468@cornell.edu>
2017-06-14 17:33:38 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    setup(
+        name='maggma',
+        version='0.0.1',
+        description='MongoDB aggregation tools',
+        long_description=open(os.path.join(module_dir, 'README.md')).read(),
+        url='https://github.com/materialsproject/maggma',
+        author='MP Team',
+        license='modified BSD',
+        packages=find_packages(),
+        package_data={},
+        zip_safe=False,
+        install_requires=['pymongo>=3.4.0'],
+        classifiers=['Programming Language :: Python :: 2.7',
+                     "Programming Language :: Python :: 3",
+                     "Programming Language :: Python :: 3.6",
+                     'Development Status :: 5 - Alpha',
+                     'Intended Audience :: Science/Research',
+                     'Intended Audience :: System Administrators',
+                     'Intended Audience :: Information Technology',
+                     'Operating System :: OS Independent',
+                     'Topic :: Other/Nonlisted Topic',
+                     'Topic :: Scientific/Engineering'],
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        scripts=[os.path.join('scripts', f) for f in os.listdir(os.path.join(module_dir, 'scripts'))]
+    )
