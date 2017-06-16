c44566e55a2f99613620c742ff218341d85526ff
Gilberto Pastorello <gzpastorello@lbl.gov>
2017-06-16 12:38:33 -0700

    add lava code and tool execution script

---
+++ maggma/lava/__init__.py
@@ -0,0 +1,10 @@
+"""
+lava: Logging And VAlidation for maggma
+"""
+__author__ = "Gilberto Pastorello, Dan Gunter"
+__copyright__ = "Copyright 2012-2017, The Materials Project"
+__version__ = "1.0"
+__maintainer__ = "Dan Gunter"
+__email__ = "dkgunter@lbl.gov"
+__status__ = "Development"
+__date__ = "6/14/17"
