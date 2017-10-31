3e80e07863686fe054d65e32584c6dc65ed14601
shyamd <shyamd@lbl.gov>
2017-10-31 13:38:25 -0700

    Test for mongostore

--- maggma/tests/test_stores.py
+++ maggma/tests/test_stores.py
@@ -2,12 +2,38 @@ import os
 import unittest
 
 import mongomock.collection
+import pymongo.collection
 
 from maggma.stores import *
 
 module_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 db_dir = os.path.abspath(os.path.join(module_dir, "..", "..", "test_files", "settings_files"))
 
+
+
+class TestMongoStore(unittest.TestCase):
+
+	def setUp(self):
+		self.mongostore = MongoStore("maggma_test","test")
+
+	def test(self):
+		self.assertEqual(self.mongostore.collection,None)
+		self.mongostore.connect()
+		self.assertIsInstance(self.mongostore.collection, pymongo.collection.Collection)
+		
+		self.mongostore.collection.insert({"a":1,"b":2,"c":3})
+		self.assertEqual(self.mongostore.query(properties=["a"])[0]['a'],1)
+		self.assertEqual(self.mongostore.query(properties=["b"])[0]['b'],2)
+		self.assertEqual(self.mongostore.query(properties=["c"])[0]['c'],3)
+
+		self.mongostore.collection.insert({"a":4,"d":5,"e":6})
+		self.assertEqual(self.mongostore.distinct("a"),[1,4])
+
+		self.mongostore.update("e",[{"e":6,"d": 4}])
+		self.assertEqual(self.mongostore.query(criteria={"d":{"$exists":1}},properties=["d"])[0]["d"],4)
+
+	def tearDown(self):
+		self.mongostore.collection.drop()
 
 
 class TestMemoryStore(unittest.TestCase):
@@ -19,8 +45,5 @@ class TestMemoryStore(unittest.TestCase):
         self.assertEqual(self.memstore.collection, None)
         self.memstore.connect()
         self.assertIsInstance(self.memstore.collection, mongomock.collection.Collection)
-        self.assertEqual(self.memstore(), self.memstore.collection)
-        self.assertEqual(self.memstore.meta.name, "collection.db.collection.meta")
-
 
 
