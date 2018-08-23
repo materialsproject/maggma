352b2fcb27fffa8a8bea0372e79cb862627ac729
Matthew Horton <mkhorton@users.noreply.github.com>
2018-08-23 14:49:09 -0700

    Update Jupyter detection for tqdm in runner

--- maggma/runner.py
+++ maggma/runner.py
@@ -18,7 +18,7 @@ from maggma.utils import get_mpi, grouper, primed
 # import tqdm Jupyter widget if running inside Jupyter
 try:
     # noinspection PyUnresolvedReferences
-    if type(get_ipython().__name__) == 'ZMQInteractiveShell':
+    if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
         from tqdm import tqdm_notebook as tqdm
     else: # likely 'TerminalInteractiveShell'
         from tqdm import tqdm
