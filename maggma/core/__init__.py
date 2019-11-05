9cab45d04f8e462a7def016ac19af4c60bf18865
Shyam Dwaraknath <shyamd@lbl.gov>
2019-11-05 15:29:06 -0800

    refactor core components

---
+++ maggma/core/__init__.py
@@ -0,0 +1,3 @@
+from maggma.core.store import Store, Sort, DateTimeFormat, StoreError
+from maggma.core.builder import Builder
+from maggma.core.validator import Validator
---
+++ maggma/core/builder.py
@@ -0,0 +1,139 @@
+# coding: utf-8
+"""
+Module containing the core builder definition
+"""
+from __future__ import annotations
+
+import logging
+from abc import ABCMeta, abstractmethod
+from typing import Union, Optional, Dict, List, Iterator, Any
+
+from monty.json import MSONable, MontyDecoder
+from maggma.utils import grouper
+from maggma.core import Store
+
+
+class Builder(MSONable, metaclass=ABCMeta):
+    """
+    Base Builder class
+    At minimum this class should implement:
+    get_items - Get items from the sources
+    update_targets - Updates the sources with results
+
+    Multiprocessing and MPI processing can be used if all
+    the data processing is  limited to process_items
+    """
+
+    def __init__(
+        self,
+        sources: Union[List[Store], Store],
+        targets: Union[List[Store], Store],
+        chunk_size: int = 1000,
+        query: Optional[Dict] = None,
+    ):
+        """
+        Initialize the builder the framework.
+
+        Args:
+            sources: source Store(s)
+            targets: target Store(s)
+            chunk_size: chunk size for processing
+            query: dictionary of options to utilize on a source;
+                   Each builder has internal logic on which souce this will apply to
+        """
+        self.sources = sources if isinstance(sources, list) else [sources]
+        self.targets = targets if isinstance(targets, list) else [targets]
+        self.chunk_size = chunk_size
+        self.query = query
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.addHandler(logging.NullHandler())
+
+    def connect(self):
+        """
+        Connect to the builder sources and targets.
+        """
+        stores = self.sources + self.targets
+        for s in stores:
+            s.connect()
+
+    @abstractmethod
+    def get_items(self) -> Iterator:
+        """
+        Returns all the items to process.
+
+        Returns:
+            generator or list of items to process
+        """
+        pass
+
+    def process_item(self, item: Any) -> Any:
+        """
+        Process an item. Should not expect DB access as this can be run MPI
+        Default behavior is to return the item.
+        Args:
+            item:
+
+        Returns:
+           item: an item to update
+        """
+        return item
+
+    @abstractmethod
+    def update_targets(self, items: List):
+        """
+        Takes a dictionary of targets and items from process item and updates them
+        Can also perform other book keeping in the process such as storing gridfs oids, etc.
+
+        Args:
+            items:
+
+        Returns:
+
+        """
+        pass
+
+    def finalize(self, cursor=None):
+        """
+        Perform any final clean up.
+        """
+        # Close any Mongo connections.
+        for store in self.sources + self.targets:
+            try:
+                store.collection.database.client.close()
+            except AttributeError:
+                continue
+        # Runner will pass iterable yielded by `self.get_items` as `cursor`. If
+        # this is a Mongo cursor with `no_cursor_timeout=True` (not the
+        # default), we must be explicitly kill it.
+        try:
+            cursor and cursor.close()
+        except AttributeError:
+            pass
+
+    def run(self):
+        """
+        Run the builder serially
+
+        Args:
+            builder_id (int): the index of the builder in the builders list
+        """
+        self.connect()
+
+        cursor = self.get_items()
+
+        for chunk in grouper(cursor, self.chunk_size):
+            self.logger.info("Processing batch of {} items".format(self.chunk_size))
+            processed_items = [
+                self.process_item(item) for item in chunk if item is not None
+            ]
+            self.update_targets(processed_items)
+
+        self.finalize(cursor)
+
+    def __getstate__(self):
+        return self.as_dict()
+
+    def __setstate__(self, d):
+        d = {k: v for k, v in d.items() if not k.startswith("@")}
+        d = MontyDecoder().process_decoded(d)
+        self.__init__(**d)
