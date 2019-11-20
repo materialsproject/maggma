1046be4cab0848aa469116abced24013398921dc
Shyam Dwaraknath <shyamd@lbl.gov>
2019-11-20 09:13:15 -0800

    upgrade to pytest

--- maggma/tests/test_utils.py
+++ maggma/tests/test_utils.py
@@ -1,33 +1,33 @@
 # coding: utf-8
 """
-Tests utilities
+Tests for builders
 """
-import unittest
+import pytest
 
 from maggma.utils import recursive_update, Timeout
 from time import sleep
 
-class UtilsTests(unittest.TestCase):
 
-    def test_recursiveupdate(self):
-        d = {"a": {"b": 3}, "c": [4]}
+def test_recursiveupdate():
+    d = {"a": {"b": 3}, "c": [4]}
 
-        recursive_update(d, {"c": [5]})
-        self.assertEqual(d["c"], [5])
+    recursive_update(d, {"c": [5]})
+    assert d["c"] ==  [5]
 
-        recursive_update(d, {"a": {"b": 5}})
-        self.assertEqual(d["a"]["b"], 5)
+    recursive_update(d, {"a": {"b": 5}})
+    assert d["a"]["b"] ==  5
 
-        recursive_update(d, {"a": {"b": [6]}})
-        self.assertEqual(d["a"]["b"], [6])
+    recursive_update(d, {"a": {"b": [6]}})
+    assert d["a"]["b"] ==  [6]
 
-        recursive_update(d, {"a": {"b": [7]}})
-        self.assertEqual(d["a"]["b"], [7])
+    recursive_update(d, {"a": {"b": [7]}})
+    assert d["a"]["b"] ==  [7]
 
-    def test_timeout(self):
+def test_timeout():
 
-        def takes_too_long():
-            with Timeout(seconds=1):
-                sleep(2)
+    def takes_too_long():
+        with Timeout(seconds=1):
+            sleep(2)
+    with pytest.raises(TimeoutError):
+        takes_too_long()
 
-        self.assertRaises(TimeoutError, takes_too_long)
