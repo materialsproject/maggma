21e53d5e58244871c9e69c5a947d347c91ca09bb
Shyam Dwaraknath <shyamd@lbl.gov>
2020-01-02 14:52:39 -0800

    module level import

--- src/maggma/api/__init__.py
+++ src/maggma/api/__init__.py
@@ -3,3 +3,6 @@ from monty.serialization import loadfn
 from pathlib import Path
 
 default_error_responses = loadfn(Path(__file__).parent / "default_responses.yaml")
+
+from maggma.api.endpoint_cluster import EndpointCluster
+from maggma.api.cluster_manager import ClusterManager
