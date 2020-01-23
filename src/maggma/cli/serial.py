6e451eeb9f925da528a39f97fa4657ce5a992e32
Shyam Dwaraknath <shyamd@lbl.gov>
2020-01-22 22:10:10 -0800

    include total

--- src/maggma/cli/serial.py
+++ src/maggma/cli/serial.py
@@ -2,9 +2,10 @@
 # coding utf-8
 
 import logging
+from types import GeneratorType
 from tqdm import tqdm
 
-from maggma.utils import grouper
+from maggma.utils import grouper, primed
 from maggma.core import Builder
 
 
@@ -21,6 +22,21 @@ def serial(builder: Builder):
 
     for chunk in grouper(tqdm(cursor), builder.chunk_size):
         logger.info("Processing batch of {} items".format(builder.chunk_size))
+    total = None
+    if isinstance(cursor, GeneratorType):
+        try:
+            cursor = primed(cursor)
+            if hasattr(builder, "total"):
+                total = builder.total
+        except StopIteration:
+            pass
+
+    elif hasattr(cursor, "__len__"):
+        total = len(cursor)
+    elif hasattr(cursor, "count"):
+        total = cursor.count()
+
+    for chunk in grouper(tqdm(cursor, total=total), builder.chunk_size):
         processed_items = [builder.process_item(item) for item in chunk]
         builder.update_targets(processed_items)
 
