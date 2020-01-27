5bab7a192f0fbbb5860e2c82500c93518df018c3
Shyam Dwaraknath <shyamd@lbl.gov>
2020-01-27 13:01:41 -0800

    implementation for compounds stores

--- tests/stores/test_compound_stores.py
+++ tests/stores/test_compound_stores.py
@@ -62,6 +62,11 @@ def jointstore(jointstore_test1, jointstore_test2):
     return store
 
 
+def test_joint_store_count(jointstore):
+    assert jointstore.count() == 10
+    assert jointstore.count({"test2.category2": {"$exists": 1}}) == 5
+
+
 def test_joint_store_query(jointstore):
     # Test query all
     docs = list(jointstore.query())
@@ -191,6 +196,12 @@ def test_concat_store_distinct(concat_store):
 def test_concat_store_groupby(concat_store):
     assert len(list(concat_store.groupby("index"))) == 4
     assert len(list(concat_store.groupby("task_id"))) == 40
+
+
+def test_concat_store_count(concat_store):
+
+    assert concat_store.count() == 40
+    assert concat_store.count({"prop": "3"}) == 4
 
 
 def test_concat_store_query(concat_store):
