aa114ddc2cd5bc6f2fd8d0e5f964690e7080e8f8
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-13 15:10:34 -0700

    code cleanup

--- src/maggma/api/query_operator.py
+++ src/maggma/api/query_operator.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Optional
 from pydantic import BaseModel
 from fastapi import Query
 from monty.json import MSONable
+from maggma.core import Store
 from maggma.api.util import STORE_PARAMS, dynamic_import
 import inspect
 
@@ -18,9 +19,13 @@ class QueryOperator(MSONable):
         """
         raise NotImplementedError("Query operators must implement query")
 
-    def meta(self) -> Dict:
+    def meta(self, store: Store, query: Dict) -> Dict:
         """
         Returns meta data to return with the Response
+
+        Args:
+            store: the Maggma Store that the resource uses
+            query: the query being executed in this API call
         """
         return {}
 
@@ -69,7 +74,7 @@ class PaginationQuery(QueryOperator):
 
         self.query = query
 
-    def meta(self) -> Dict:
+    def meta(self, store: Store, query: Dict) -> Dict:
         """
         Metadata for the pagination params
         """
@@ -91,8 +96,6 @@ class SparseFieldsQuery(QueryOperator):
         self.model = model
 
         model_fields = list(self.model.__fields__.keys())
-        # print(model_fields)
-        # print(default_fields)
         self.default_fields = (
             model_fields if default_fields is None else list(default_fields)
         )
@@ -108,7 +111,7 @@ class SparseFieldsQuery(QueryOperator):
                 default_fields_string,
                 description=f"Fields to project from {model.__name__} as a list of comma seperated strings",
             ),
-            all_fields: bool = Query(True, description="Include all fields."),
+            all_fields: bool = Query(False, description="Include all fields."),
         ) -> STORE_PARAMS:
             """
             Pagination parameters for the API Endpoint
@@ -122,7 +125,7 @@ class SparseFieldsQuery(QueryOperator):
 
         self.query = query
 
-    def meta(self) -> Dict:
+    def meta(self, store: Store, query: Dict) -> Dict:
         """
         Returns metadata for the Sparse field set
         """
@@ -139,7 +142,9 @@ class SparseFieldsQuery(QueryOperator):
 
     @classmethod
     def from_dict(cls, d):
-
+        """
+        Special from_dict to autoload the pydantic model from the location string
+        """
         model = d.get("model")
         if isinstance(model, str):
             module_path = ".".join(model.split(".")[:-1])
@@ -158,35 +163,31 @@ class DefaultDynamicQuery(QueryOperator):
     def __init__(
         self,
         model: BaseModel,
-        additional_signature_fields=None,
-        supported_types: list = None,
-        query_mapping: dict = None,
+        additional_signature_fields=None,  # TODO field types -> name, type, default value
     ):
+        """
+        TODO: better description of this query operator with what it supports: types, search patterns, etc.
+        Args:
+            model: PyDantic Model to base the query language on
+
+        """
         self.model = model
 
-        self.supported_types = (
-            supported_types if supported_types is not None else [str, int, float]
-        )
-
-        if query_mapping is None:
-            self.query_mapping = {
-                "eq": "$eq",
-                "not_eq": "$ne",
-                "lt": "$lt",
-                "gt": "$gt",
-                "in": "$in",
-                "not_in": "$nin",
-            }
-            query_mapping = self.query_mapping
-        else:
-            self.query_mapping = query_mapping
+        self.query_mapping = {
+            "eq": "$eq",  # TODO: don't need _eq for equals
+            "not_eq": "$ne",
+            "lt": "$lt",
+            "gt": "$gt",
+            "in": "$in",
+            "not_in": "$nin",
+        }  # TODO: hardcode back into the main query generation function
 
         self.additional_signature_fields = additional_signature_fields
         if additional_signature_fields is None:
             self.additional_signature_fields = dict()
 
-            # construct fields
-            # find all fields in data_object
+        # construct fields
+        # find all fields in data_object
         all_fields = list(model.__fields__.items())
 
         # turn fields into operators, also do type checking
@@ -202,13 +203,12 @@ class DefaultDynamicQuery(QueryOperator):
                 if v is not None:
                     name, operator = k.split("_", 1)
                     try:
-                        crit[name] = {query_mapping[operator]: v}
+                        crit[name] = {self.query_mapping[operator]: v}
                     except KeyError:
                         raise KeyError(
                             f"Cannot find key {k} in current query to database mapping"
                         )
             return {"criteria": crit}
-            # TODO ask shyam about this part, how to let it return something compatible to STORE_PARAMS
 
         # building the signatures for FastAPI Swagger UI
         signatures = []
@@ -228,7 +228,7 @@ class DefaultDynamicQuery(QueryOperator):
     def fields_to_operator(self, all_fields):
         params = dict()
         for name, model_field in all_fields:
-            if model_field.type_ in self.supported_types:
+            if model_field.type_ in [str, int, float]:
                 params[f"{model_field.name}_eq"] = [
                     model_field.type_,
                     Query(
--- src/maggma/api/resource.py
+++ src/maggma/api/resource.py
@@ -25,10 +25,6 @@ class Resource(MSONable):
         This class provides a number of convenience features
         including full pagination, field projection, and the
         MAPI query lanaugage
-
-        - implements custom error handlers to provide MAPI Responses
-        - implement standard metadata response for class
-        - JSON Configuration
         """
 
     def __init__(
@@ -65,14 +61,12 @@ class Resource(MSONable):
         else:
             self.model = model
 
-        # print(self.store.key)
-
         self.query_operators = (
             query_operators
             if query_operators is not None
             else [
                 PaginationQuery(),
-                SparseFieldsQuery(self.model, default_fields=[self.store.key],),
+                SparseFieldsQuery(self.model, default_fields=[self.store.key]),
                 DefaultDynamicQuery(self.model),
             ]
         )
@@ -86,17 +80,8 @@ class Resource(MSONable):
         Internal method to prepare the endpoint by setting up default handlers
         for routes
         """
-        self.set_root()
-        # self.build_get_by_key()
+        self.build_get_by_key()
         self.set_dynamic_model_search()
-
-    def set_root(self):
-        async def get_root():
-            return RedirectResponse("/docs")
-
-        self.router.get(
-            "/", response_description="Root level of this resource", tags=self.tags,
-        )(get_root)
 
     def build_get_by_key(self):
         key_name = self.store.key
@@ -121,9 +106,6 @@ class Resource(MSONable):
             """
             self.store.connect()
 
-            query = {self.store.key: key}
-
-            elements = self.store.distinct("elements", query)
             item = self.store.query_one(
                 criteria={self.store.key: key}, properties=fields["properties"]
             )
@@ -134,13 +116,12 @@ class Resource(MSONable):
                     detail=f"Item with {self.store.key} = {key} not found",
                 )
 
-            response = Response(data=[item])
-            response.meta.elements = elements
+            response = {"data": [item], "meta": Meta()}
 
-            return response.dict()
+            return response
 
         self.router.get(
-            f"/{{{key_name}}}",
+            f"/{{{key_name}}}/",
             response_description=f"Get an {model_name} by {key_name}",
             response_model=self.response_model,
             response_model_exclude_unset=True,
@@ -158,10 +139,9 @@ class Resource(MSONable):
 
             count_query = query["criteria"]
             count = self.store.count(count_query)
-            elements = self.store.distinct("elements", count_query)
-            data = [self.model(**d) for d in self.store.query(**query)]
-            meta = Meta(total=count, elements=elements)
-            response = Response[self.model](data=data, meta=meta.dict())
+            data = list(self.store.query(**query))
+            meta = Meta(total=count)
+            response = {"data": data, "meta": meta.dict()}
             return response
 
         attach_signature(
@@ -176,10 +156,10 @@ class Resource(MSONable):
         )
 
         self.router.get(
-            "/search",
+            "/",
             tags=self.tags,
             summary=f"Get {model_name} documents",
-            # response_model=self.response_model,
+            response_model=self.response_model,
             response_description=f"Search for a {model_name}",
             response_model_exclude_unset=True,
         )(search)
