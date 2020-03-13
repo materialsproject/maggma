a2f11d492eff6f6d9d4eca1dcfe67232e05d0a2c
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-13 14:04:07 -0700

    remove version in submodule

--- src/maggma/api/__init__.py
+++ src/maggma/api/__init__.py
@@ -1,8 +1,1 @@
 """ Simple API Interface for Maggma """
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
