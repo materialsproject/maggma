6943f5dec9748b8c90e0a0fddf9e2ccb96409880
Shyam Dwaraknath <shyamd@lbl.gov>
2020-04-06 12:14:35 -0700

    update tests

--- tests/cli/test_multiprocessing.py
+++ tests/cli/test_multiprocessing.py
@@ -1,7 +1,12 @@
 import pytest
 import time
 import asyncio
-from maggma.cli.multiprocessing import AsyncBackPressuredMap, grouper
+from maggma.cli.multiprocessing import (
+    grouper,
+    BackPressure,
+    AsyncUnorderedMap,
+    safe_dispatch,
+)
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -14,9 +19,6 @@ async def test_grouper():
     async for group in grouper(arange(100), n=10):
         assert len(group) == 10
 
-    async for group in grouper(arange(9), n=10, fillvalue="s"):
-        assert len(group) == 10
-
     async for group in grouper(arange(9), n=10):
         assert len(group) == 9
 
@@ -26,30 +28,46 @@ def wait_and_return(x):
     return x * x
 
 
+async def arange(n):
+    for num in range(n):
+        yield num
+
+
 @pytest.mark.asyncio
-async def test_backpressure_map():
+async def test_backpressure():
 
-    executor = ThreadPoolExecutor(1)
-    mapper = AsyncBackPressuredMap(
-        iterator=range(3), func=wait_and_return, max_run=2, executor=executor
-    )
-
-    true_values = [x * x for x in range(3)]
-    async for finished_val in mapper:
-        finished_val = await finished_val
-        assert finished_val.result() == true_values.pop(0)
-
-    mapper = AsyncBackPressuredMap(
-        iterator=range(3), func=wait_and_return, max_run=2, executor=executor
-    )
+    iterable = range(10)
+    backpressure = BackPressure(iterable, 2)
 
     # Put two items into the process queue
-    futures = [await mapper.__anext__(), await mapper.__anext__()]
+    asyncio.gather(backpressure.__anext__(), backpressure.__anext__())
+
     # Ensure back_pressure enabled
-    assert mapper.back_pressure.locked()
-    await asyncio.sleep(2)
-    # Ensure back_pressure enabled till data is dequeued from process_pipeline
-    assert mapper.back_pressure.locked()
-    # Dequeue futures and ensure back_pressure is gone
-    await asyncio.gather(*futures)
-    assert not mapper.back_pressure.locked()
+    assert backpressure.back_pressure.locked()
+
+    releaser = backpressure.release(arange(10))
+    await releaser.__anext__()
+    assert not backpressure.back_pressure.locked()
+    asyncio.gather(releaser.__anext__(), releaser.__anext__())
+    assert not backpressure.back_pressure.locked()
+
+
+@pytest.mark.asyncio
+async def test_async_map():
+
+    executor = ThreadPoolExecutor(1)
+    amap = AsyncUnorderedMap(wait_and_return, arange(3), executor)
+    true_values = {x * x for x in range(3)}
+
+    finished_vals = set()
+    async for finished_val in amap:
+        finished_vals.add(finished_val)
+
+    assert finished_vals == true_values
+
+
+def test_safe_dispatch():
+    def bad_func(val):
+        raise ValueError("AAAH")
+
+    safe_dispatch((bad_func, ""))
