5c961768c47e6e4c2681a3c63d42c431ae344b6b
Shyam Dwaraknath <shyamd@lbl.gov>
2020-05-02 09:47:01 -0700

    Add logging fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import sys
 import pytest
+import logging
 
 
 @pytest.fixture
@@ -14,3 +16,17 @@ def db_json(test_dir):
     db_dir = test_dir / "settings_files"
     db_json = db_dir / "db.json"
     return db_json.resolve()
+
+
+@pytest.fixture
+def log_to_stdout():
+    # Set Logging
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    return root
