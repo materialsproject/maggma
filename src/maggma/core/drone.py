731c224b6d62e9e73319257d08f53905edf457a6
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-05-02 12:45:33 -0700

    Initial Drone Implementation (#145)
    * drone added
    * tests added and pass locally
    * listing passes
    * drone docs added

---
+++ src/maggma/core/drone.py
@@ -0,0 +1,217 @@
+from pydantic import BaseModel, Field
+from pathlib import PosixPath, Path
+from datetime import datetime
+from typing import Dict, List, Optional, Iterable
+import hashlib
+from abc import abstractmethod
+import os
+from maggma.core import Builder
+
+
+class Document(BaseModel):
+    """
+    Represent a file
+    """
+
+    path: PosixPath = Field(..., title="Path of this file")
+    name: str = Field(..., title="File name")
+
+
+class RecordIdentifier(BaseModel):
+    """
+    The meta data for a record
+    """
+
+    last_updated: datetime = Field(
+        ..., title="The time in which this record is last updated"
+    )
+    documents: List[Document] = Field(
+        [], title="List of documents this RecordIdentifier indicate"
+    )
+    record_key: str = Field(
+        ...,
+        title="Hash that uniquely define this record, can be inferred from each document inside",
+    )
+    state_hash: Optional[str] = Field(
+        None, title="Hash of the state of the documents in this Record"
+    )
+
+    @property
+    def parent_directory(self) -> Path:
+        """
+        root most directory that documnents in this record share
+        :return:
+        """
+        paths = [doc.path.as_posix() for doc in self.documents]
+        return Path(os.path.commonprefix(paths))
+
+    def compute_state_hash(self) -> str:
+        """
+        compute the hash of the state of the documents in this record
+        :param doc_list: list of documents
+        :return:
+            hash of the list of documents passed in
+        """
+        digest = hashlib.md5()
+        for doc in self.documents:
+            digest.update(doc.name.encode())
+            with open(doc.path.as_posix(), "rb") as file:
+                buf = file.read()
+                digest.update(buf)
+        return str(digest.hexdigest())
+
+
+class Drone(Builder):
+    """
+    An abstract drone that handles operations with database, using Builder to support multi-threading
+    User have to implement all abstract methods to specify the data that they want to use to interact with this class
+
+    Note: All embarrassingly parallel function should be in process_items
+    Note: The steps of a Drone can be divided into roughly 5 steps
+
+     For sample usage, please see /docs/getting_started/core_drone.md
+     and example implementation is available in tests/builders/test_simple_bibdrone.py
+    """
+
+    def __init__(self, store, path: Path):
+        self.store = store
+        self.path = path
+        super().__init__(sources=[], targets=store)
+
+    @abstractmethod
+    def compute_record_identifier_key(self, doc: Document) -> str:
+        """
+        Compute the RecordIdentifier key that this document correspond to
+
+        Args:
+            doc: document which the record identifier key will be inferred from
+
+        Returns:
+            RecordIdentifiierKey
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def read(self, path: Path) -> List[RecordIdentifier]:
+        """
+        Given a folder path to a data folder, read all the files, and return a dictionary
+        that maps each RecordKey -> [RecordIdentifier]
+
+        ** Note: require user to implement the function computeRecordIdentifierKey
+
+        Args:
+            path: Path object that indicate a path to a data folder
+
+        Returns:
+            List of Record Identifiers
+        """
+        pass
+
+    @abstractmethod
+    def compute_data(self, recordID: RecordIdentifier) -> Dict:
+        """
+        User can specify what raw data they want to save from the Documents that this recordID refers to
+
+        Args:
+            recordID: recordID that needs to be re-saved
+
+        Returns:
+            Dictionary of NAME_OF_DATA -> DATA
+                        ex:
+                            for a recordID refering to 1,
+                            {
+                                "citation": cite.bibtex ,
+                                "text": data.txt
+                            }
+        """
+        pass
+
+    def should_update_records(
+        self, record_identifiers: List[RecordIdentifier]
+    ) -> List[RecordIdentifier]:
+        """
+        Batch query database by computing all the keys and send them at once
+
+        Args:
+            record_identifiers: all the record_identifiers that need to fetch from database
+
+        Returns:
+            List of recordIdentifiers representing data that needs to be updated
+        """
+        cursor = self.store.query(
+            criteria={
+                "record_key": {"$in": [r.record_key for r in record_identifiers]}
+            },
+            properties=["record_key", "state_hash", "last_updated"],
+        )
+
+        not_exists = object()
+        db_record_log = {doc["record_key"]: doc["state_hash"] for doc in cursor}
+        to_update_list = [
+            recordID.state_hash != db_record_log.get(recordID.record_key, not_exists)
+            for recordID in record_identifiers
+        ]
+        return [
+            recordID
+            for recordID, to_update in zip(record_identifiers, to_update_list)
+            if to_update
+        ]
+
+    def assimilate(self, path: Path) -> List[RecordIdentifier]:
+        """
+        Function mainly for debugging purpose. It will
+            1. read file in the path specified
+            2. convert them into recordIdentifier
+            3. return the converted recordIdentifiers
+        Args:
+            path: path in which files are read
+
+        Returns:
+            list of record Identifiers
+        """
+        record_identifiers: List[RecordIdentifier] = self.read(path=path)
+        return record_identifiers
+
+    def get_items(self) -> Iterable:
+        """
+        Read from the path that was given, compare against database files to find recordIDs that needs to be updated
+
+        Returns:
+            RecordIdentifiers that needs to be updated
+        """
+        self.logger.debug(
+            "Starting get_items in {} Builder".format(self.__class__.__name__)
+        )
+        record_identifiers: List[RecordIdentifier] = self.read(path=self.path)
+        records_to_update = self.should_update_records(record_identifiers)
+        return records_to_update
+
+    def update_targets(self, items: List):
+        """
+        Receive a list of items to update, update the items
+
+        Assume that each item are in the correct format
+        Args:
+            items: List of items to update
+
+        Returns:
+            None
+        """
+        if len(items) > 0:
+            self.logger.debug("Updating {} items".format(len(items)))
+            self.store.update(items)
+        else:
+            self.logger.debug("There are no items to update")
+
+    def process_item(self, item: RecordIdentifier) -> Dict:  # type: ignore
+        """
+        compute the item to update
+
+        Args:
+            item: item to update
+
+        Returns:
+            result from expanding the item
+        """
+
+        return {**self.compute_data(recordID=item), **item.dict()}
---
+++ tests/builders/test_simple_bib_drone.py
@@ -0,0 +1,128 @@
+from .simple_bib_drone import SimpleBibDrone
+from maggma.stores import MongoStore
+from pathlib import Path
+import pytest
+from datetime import datetime
+
+
+@pytest.fixture
+def init_drone(test_dir):
+    """
+    Initialize the drone, do not initialize the connection with the database
+
+    :return:
+        initialized drone
+    """
+    mongo_store = MongoStore(
+        database="drone_test", collection_name="drone_test", key="record_key"
+    )
+    simple_path = test_dir / "simple_bib_example_data"
+    assert simple_path.exists(), f"{simple_path} not found"
+    simple_bib_drone = SimpleBibDrone(store=mongo_store, path=simple_path)
+    return simple_bib_drone
+
+
+def test_read(init_drone: SimpleBibDrone):
+    """
+    Test whether read function is correct
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+        None
+    """
+    list_record_id = init_drone.read(init_drone.path)
+    assert len(list_record_id) == 7
+    state_hashes = [r.state_hash for r in list_record_id]
+    assert len(state_hashes) == len(list_record_id)  # all record_id has hash
+    assert len((set(state_hashes))) == len(state_hashes)  # all unique hashes
+
+    num_docs = sum([len(r.documents) for r in list_record_id])
+    assert num_docs == 12
+
+
+def test_record_id(init_drone: SimpleBibDrone):
+    """
+    Test validity of RecordIdentifier
+
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+        None
+    """
+    list_record_id = init_drone.read(init_drone.path)
+    record0 = list_record_id[0]
+    assert record0.parent_directory == init_drone.path
+    assert record0.last_updated < datetime.now()
+    assert len(record0.documents) > 0
+    # state hash does not change when the file is not changed
+    assert record0.compute_state_hash() == record0.state_hash
+
+
+def test_process_item(init_drone: SimpleBibDrone):
+    """
+    Test whether data is expaneded correctly and whether meta data is added
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+        None
+    """
+    list_record_id = init_drone.read(init_drone.path)
+    data = init_drone.process_item(list_record_id[0])
+    assert "citations" in data
+    assert "text" in data
+    assert "record_key" in data
+    assert "last_updated" in data
+    assert "documents" in data
+    assert "state_hash" in data
+
+
+def test_compute_record_identifier_key(init_drone: SimpleBibDrone):
+    list_record_id = init_drone.read(init_drone.path)
+    record0 = list_record_id[0]
+    doc0 = record0.documents[0]
+    assert record0.record_key == init_drone.compute_record_identifier_key(doc0)
+
+
+def test_get_items(init_drone: SimpleBibDrone):
+    """
+    This test might take a while
+    test whether get_items work correctly.
+    It should fetch from database all the files that needs to be updated
+
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+        None
+    """
+
+    init_drone.connect()
+    init_drone.run()  # make sure the database is up-to-date
+    init_drone.connect()
+    assert sum([1 for _ in init_drone.get_items()]) == 0
+    init_drone.finalize()
+
+    init_drone.connect()
+    init_drone.store.remove_docs(criteria={})  # clears the database
+    assert sum([1 for _ in init_drone.get_items()]) == 7
+    init_drone.finalize()
+
+
+def test_assimilate(init_drone: SimpleBibDrone):
+    """
+    Test whether assimilate file is correct
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+    None
+    """
+    record_ids = init_drone.assimilate(init_drone.path)
+    assert len(record_ids) == 7
+
+
+def test_compute_data(init_drone: SimpleBibDrone):
+    """
+    test whether data is extracted as expected
+
+    :param init_drone: un-connected simpleBibDrone instance
+    :return:
+        None
+    """
+    list_record_id = init_drone.read(init_drone.path)
+    data = init_drone.process_item(list_record_id[0])
+    assert "citations" in data
+    assert "text" in data
