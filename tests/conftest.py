3240f3b29cd4e4d2e715ce577301311eaf80fed9
Shyam Dwaraknath <shyamd@lbl.gov>
2020-05-02 19:31:07 -0700

    clean up test output

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,19 @@ def log_to_stdout():
     ch.setFormatter(formatter)
     root.addHandler(ch)
     return root
+
+
+def pytest_itemcollected(item):
+    """Make tests names more readable in the tests output."""
+    item._nodeid = (
+        item._nodeid.replace(".py", "")
+        .replace("tests/", "")
+        .replace("test_", "")
+        .replace("_", " ")
+        .replace("Test", "")
+        .replace("Class", " class")
+        .lower()
+    )
+    doc = item.obj.__doc__.strip() if item.obj.__doc__ else ""
+    if doc:
+        item._nodeid = item._nodeid.split("::")[0] + "::" + doc
