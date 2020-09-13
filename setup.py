9da21f3bbf3c58c162cf407cae9a23409670764c
Shyam D <shyamd@lbl.gov>
2020-09-13 15:49:53 -0700

    update requirements for nbformat

--- setup.py
+++ setup.py
@@ -43,7 +43,11 @@ setup(
         "sshtunnel>=0.1.5",
         "msgpack-python>=0.5.6",
     ],
-    extras_require={"vault": ["hvac>=0.9.5"], "S3": ["boto3==1.14.56"]},
+    extras_require={
+        "vault": ["hvac>=0.9.5"],
+        "S3": ["boto3==1.14.56"],
+        "notebook_runner": ["IPython>=7.16", "nbformat>=5.0"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
