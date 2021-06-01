71fee7bde4f04c640743049f7059d8b188222d79
Jason Munro <jmunro@lbl.gov>
2021-06-01 14:50:19 -0700

    Add aggregation pipeline resource

--- src/maggma/api/resource/__init__.py
+++ src/maggma/api/resource/__init__.py
@@ -1,3 +1,4 @@
 from maggma.api.resource.core import Resource
 from maggma.api.resource.read_resource import ReadOnlyResource, attach_query_ops
 from maggma.api.resource.user_data import UserSubmissionResource
+from maggma.api.resource.aggregation import AggregationResource
