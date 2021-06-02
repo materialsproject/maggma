a53d3d31e273937ef458e3388afc8b90a7a5c3a1
Jason Munro <jmunro@lbl.gov>
2021-06-02 10:45:57 -0700

    Import post resource

--- src/maggma/api/resource/__init__.py
+++ src/maggma/api/resource/__init__.py
@@ -2,3 +2,4 @@ from maggma.api.resource.core import Resource
 from maggma.api.resource.read_resource import ReadOnlyResource, attach_query_ops
 from maggma.api.resource.user_data import UserSubmissionResource
 from maggma.api.resource.aggregation import AggregationResource
+from maggma.api.resource.post_resource import PostOnlyResource
