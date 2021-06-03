e53dca61ba68634d8c4685e9fe38ed7c98f121ab
Jason Munro <jmunro@lbl.gov>
2021-06-03 14:11:04 -0700

    Fix resource imports

--- src/maggma/api/resource/__init__.py
+++ src/maggma/api/resource/__init__.py
@@ -1,5 +1,5 @@
 from maggma.api.resource.core import Resource
 from maggma.api.resource.read_resource import ReadOnlyResource, attach_query_ops
-from maggma.api.resource.user_data import SubmissionResource
+from maggma.api.resource.submission import SubmissionResource
 from maggma.api.resource.aggregation import AggregationResource
 from maggma.api.resource.post_resource import PostOnlyResource
