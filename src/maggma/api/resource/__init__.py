fc970c1dd6d041597fb2759cffaf80a9926a35ff
Shyam D <shyamd@lbl.gov>
2021-06-18 13:29:39 -0700

    fix isort issue

--- src/maggma/api/resource/__init__.py
+++ src/maggma/api/resource/__init__.py
@@ -1,5 +1,9 @@
-from maggma.api.resource.aggregation import AggregationResource
+# isort: off
 from maggma.api.resource.core import Resource
+
+# isort: on
+
+from maggma.api.resource.aggregation import AggregationResource
 from maggma.api.resource.post_resource import PostOnlyResource
 from maggma.api.resource.read_resource import ReadOnlyResource, attach_query_ops
 from maggma.api.resource.submission import SubmissionResource
