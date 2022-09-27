2c8c0f44a43a8396a6f9088aa9f7d4c03d9baba4
Jason Munro <jmunro@lbl.gov>
2022-09-26 21:02:00 -0700

    Update high water mark

--- src/maggma/cli/distributed.py
+++ src/maggma/cli/distributed.py
@@ -47,6 +47,7 @@ def manager(
     logger.info(f"Binding to Manager URL {url}:{port}")
     context = zmq.Context()
     context.setsockopt(opt=zmq.SocketOption.ROUTER_MANDATORY, value=1)
+    context.set_hwm(0)
     socket = context.socket(zmq.ROUTER)
     socket.bind(f"{url}:{port}")
 
