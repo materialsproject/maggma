220eecaa99daef43b4b60442785446312ae8a9c0
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-06-06 15:32:43 -0700

    Update cli settings to get system's temp directory to dump .bin files for memory profiler

--- src/maggma/cli/settings.py
+++ src/maggma/cli/settings.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+import platform
+import tempfile
+
 from pydantic import BaseSettings, Field
+
+tempdir = "/tmp" if platform.system() == "Darwin" else tempfile.gettempdir()
 
 
 class CLISettings(BaseSettings):
-
     WORKER_TIMEOUT: int = Field(
         3600,
         description="Timeout in seconds for a distributed worker",
@@ -11,6 +16,11 @@ class CLISettings(BaseSettings):
     MANAGER_TIMEOUT: int = Field(
         900,
         description="Timeout in seconds for the worker manager",
+    )
+
+    TEMP_DIR: str = Field(
+        tempdir,
+        description="Directory that memory profile .bin files are dumped to",
     )
 
     class Config:
