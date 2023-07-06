d19bd93b4673ce0c1ce974b4a9fa4ec155565859
Jason <jmunro@lbl.gov>
2023-07-06 13:29:30 -0700

    Fix builders init

---
+++ src/maggma/builders/__init__.py
@@ -0,0 +1,5 @@
+from maggma.builders.group_builder import GroupBuilder
+from maggma.builders.map_builder import CopyBuilder, MapBuilder
+from maggma.core import Builder
+
+__all__ = ["GroupBuilder", "CopyBuilder", "MapBuilder", "Builder"]
