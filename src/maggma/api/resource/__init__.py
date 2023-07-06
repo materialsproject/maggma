2d722601a9a19f50e9f09b24bd23b233b38d99e2
Jason <jmunro@lbl.gov>
2023-07-06 13:36:16 -0700

    Fix resource init

--- src/maggma/api/resource/__init__.py
+++ src/maggma/api/resource/__init__.py
@@ -1,3 +1,24 @@
 # isort: off
+from maggma.api.resource.core import Resource
+from maggma.api.resource.core import HintScheme
+from maggma.api.resource.core import HeaderProcessor
 
 # isort: on
+
+from maggma.api.resource.aggregation import AggregationResource
+from maggma.api.resource.post_resource import PostOnlyResource
+from maggma.api.resource.read_resource import ReadOnlyResource, attach_query_ops
+from maggma.api.resource.submission import SubmissionResource
+from maggma.api.resource.s3_url import S3URLResource
+
+__all__ = [
+    "Resource",
+    "HintScheme",
+    "HeaderProcessor",
+    "AggregationResource",
+    "PostOnlyResource",
+    "ReadOnlyResource",
+    "attach_query_ops",
+    "SubmissionResource",
+    "S3URLResource",
+]
