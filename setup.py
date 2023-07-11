478f38cc7f987aa35deb9e7dc031e1feac540d67
@jmmshn <jmmshn@gmail.com>
2023-07-11 13:42:03 -0700

    ruamel yaml dep typos

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "setuptools",
+        "ruamel.yaml",
         "pydantic<=1.10.11",
         "pydantic>=0.32.2",
         "pymongo>=4.2.0",
