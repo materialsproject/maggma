3b8af80e489038d8821246b6e29605673b4a9022
Ryan Kingsbury <rkingsbury@users.noreply.github.com>
2023-07-31 12:57:03 -0400

    add Filestore to __init__.py

--- src/maggma/stores/__init__.py
+++ src/maggma/stores/__init__.py
@@ -9,6 +9,7 @@ from maggma.stores.advanced_stores import (
 from maggma.stores.aws import S3Store
 from maggma.stores.azure import AzureBlobStore
 from maggma.stores.compound_stores import ConcatStore, JointStore
+from maggma.stores.file_store import FileStore
 from maggma.stores.gridfs import GridFSStore
 from maggma.stores.mongolike import (
     JSONStore,
@@ -29,6 +30,7 @@ __all__ = [
     "ConcatStore",
     "JointStore",
     "GridFSStore",
+    "FileStore",
     "JSONStore",
     "MemoryStore",
     "MongoStore",
