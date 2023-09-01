7d9ad19759a08d834493b65c26813503b80ff27f
Ryan <rkingsbury@users.noreply.github.com>
2023-09-01 09:34:32 -0400

    CI: auto dep test integration updates

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ setup(
         "montydb": ["montydb>=2.3.12"],
         "notebook_runner": ["IPython>=8.11", "nbformat>=5.0", "regex>=2020.6"],
         "azure": ["azure-storage-blob>=12.16.0", "azure-identity>=1.12.0"],
+        "testing": ["pytest", "pytest-cov", "starlette[full]"]
     },
-    tests_require=["pytest", "starlette[full]"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
