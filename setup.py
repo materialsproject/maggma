f7ae9fb874acdc97aabee465a7250fd0155cb1fa
Jason <jmunro@lbl.gov>
2023-09-07 13:56:43 -0700

    Pin pydantic

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     install_requires=[
         "setuptools",
         "ruamel.yaml<0.18",
-        "pydantic<2.0",
         "pydantic>=2.0",
         "pymongo>=4.2.0",
         "monty>=1.0.2",
